e551fb63efa503da53c7b15f8b00b5854e48ced5
weirdvector <weirdvector@gmail.com>
2016-09-03 09:48:01 -0400

    Initial commit

---
+++ pixelsorterbot.py
@@ -0,0 +1,72 @@
+# PixelSorterBot by Greg Wood, Sep 3 2016
+
+import tweepy
+from creds import creds
+from urllib import urlretrieve
+from time import sleep
+from PIL import Image
+from random import randint
+
+def get_api():
+	auth = tweepy.OAuthHandler(creds['consumer_key'], creds['consumer_secret'])
+	auth.set_access_token(creds['access_token'], creds['access_token_secret'])
+	api = tweepy.API(auth)
+	return api
+
+def get_latest_id():
+	try:
+		file = open('latest_id.txt', 'r')
+		id = int(file.read())
+		return id
+	except IOError:
+		return 0
+
+def write_latest_id(id):
+	file = open('latest_id.txt', 'w')
+	file.write(str(id))
+	file.close()
+
+def sort_picture():
+	im = Image.open('photo.jpg')
+	pixels = list(im.getdata())
+
+	color = randint(0,2)
+	rev = bool(randint(0,1))
+
+	pixels.sort(key=lambda tup:tup[color], reverse=rev)
+
+	im2 = Image.new(im.mode, im.size)
+	im2.putdata(pixels)
+	im2.save('sorted.jpg')
+
+
+def main():
+	api = get_api()
+	id = get_latest_id()
+
+	if id != 0:
+		mentions = api.mentions_timeline(id)
+	# be careful here, if the file is missing we should just get the last tweet or something
+	else:
+		mentions = api.mentions_timeline()
+
+	for mention in mentions:
+		# reply could have multiple pictures
+		for media in mention.extended_entities['media']:
+			if media['type'] == 'photo':
+
+				#download the photo
+				#add checks here for different photo types
+				urlretrieve(media['media_url_https'], 'photo.jpg')
+				sort_picture()
+
+				#post picture
+				api.update_with_media(filename='sorted.jpg', in_reply_to_status_id=id)
+
+				#wait a bit for rate limiting
+				sleep(15)
+
+		write_latest_id(mention.id)
+
+if __name__ == '__main__':
+	main()
